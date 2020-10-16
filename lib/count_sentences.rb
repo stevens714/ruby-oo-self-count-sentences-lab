332efc05e08eb36ba8952b216f279b30e18c0aed
steve <stevens714@yahoo.com>
2020-10-15 21:10:08 -0400

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size 
+    # splits string into Array, collects those would-be sentences that actually contain word characters, removes false values, then counts the remaining booleans.
+  
   end
 end
